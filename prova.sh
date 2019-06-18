b2c6a42322da73ea8ee038f61c4e6f37a5c0f8d4
p-marco <marco.petolicchio@gmail.com>
2019-06-18 08:41:06 +0200

    first new.version

---
+++ prova.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+title="$(cat item.md | grep -o 'title.*' | cut -f2- -d:)"
+author="$(cat item.md | grep -o 'author.*' | cut -f2- -d:)"
+
+
+convert ../fb-headline-new.png -size 1200x600! \
+-background none -fill black  -font Montserrat-Bold -pointsize 26 caption:"$author" \
+-pointsize 68 -gravity northwest -size 900x500! -geometry +120+200 -compose over -composite caption:"$title" \
+-gravity southwest -geometry +120-120 -compose over -composite \
+fb-banner-new.jpg
+
+
